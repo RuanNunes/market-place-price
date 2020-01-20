8cb9f52d65b0d53d71f362f7ffb0cd8b58421a86
Ruan Nunes <ruanschwanz@gmail.com>
2020-01-20 14:20:47 -0300

    Implements PersistentEntity
    Classe base para todos ostros models

---
+++ src/main/java/com/market/model/PersistentEntity.java
@@ -0,0 +1,54 @@
+package com.market.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@MappedSuperclass
+public class PersistentEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+
+	@CreationTimestamp
+	@CreatedDate
+	@Column(name = "created_date", updatable = false, nullable = false)
+	private LocalDateTime createdDate;
+
+	@UpdateTimestamp
+	@LastModifiedDate
+	@Column(name = "last_modified_date", nullable = false)
+	private LocalDateTime lastModifiedDate;
+
+	@Version
+	@Column(name = "version", nullable = false)
+	private Integer version;
+
+	@Transient
+	public boolean isPersisted() {
+		return this.getId() != null;
+	}
+
+}
--- src/main/java/com/market/model/RuleMarketPlace.java
+++ src/main/java/com/market/model/RuleMarketPlace.java
@@ -2,25 +2,22 @@ package com.market.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Entity
-public class Custumer extends PersistentEntity implements Serializable{
-
+public class RuleMarketPlace extends PersistentEntity implements Serializable{
 	private static final long serialVersionUID = 1L;
-	@NotNull
-	private String email;
-	@NotNull
-	private String password;
+	 
+	@Column(nullable = false, unique = true)
+	private String name;
 }
