c2f48612661459d6887d625710e60444cf05809c
Ruan Nunes <ruanschwanz@gmail.com>
2020-01-30 18:43:19 -0300

    correção emergencial, ajustar depois

--- src/test/java/com/market/client/RuleMarketPlaceClient.java
+++ src/test/java/com/market/client/RuleMarketPlaceClient.java
@@ -1,10 +1,8 @@
 package com.market.client;
 
-import com.market.contract.CostumerApi;
 import com.market.contract.RuleMarketPlaceApi;
-import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient(name = "rule-market-place-price", url = "${market.api.uri}")
+//@FeignClient(name = "rule-market-place-price", url = "${market.api.uri}")
 public interface RuleMarketPlaceClient extends RuleMarketPlaceApi {
 
 }
