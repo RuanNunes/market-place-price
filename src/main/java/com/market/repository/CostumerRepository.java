63ff695548279cd9cb834e98efeae3a37014994f
Ruan Nunes <ruanschwanz@gmail.com>
2020-02-10 22:05:47 -0300

    ajustes

--- src/main/java/com/market/repository/CostumerRepository.java
+++ src/main/java/com/market/repository/CostumerRepository.java
@@ -8,9 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.market.model.Costumer;
+import org.springframework.transaction.annotation.Transactional;
+
 @Repository
 public interface CostumerRepository extends JpaRepository<Costumer, Long> {
     @Query(value = "select * from costumer c" +
             " where c.name like %?1%", nativeQuery = true)
     Page<Costumer> findCostumers(String name, Pageable page);
+
+    @Transactional(readOnly = true)
+    Page<Costumer> findByNameContaining(String name, Pageable page);
 }
